388161a29728aa219d18654f6f6c1d3cf5aa4d67
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-07-12 19:52:41 -0300

    character scene

---
+++ characters/character.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+#Stats
+@export var maxHP = 100
+@export var HP = maxHP
+@export var atk = 10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
