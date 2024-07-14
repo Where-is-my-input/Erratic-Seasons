8c3d031d2045284322830e0ea214f1c91be2456a
GsX <156434433+GsX0789@users.noreply.github.com>
2024-07-14 11:38:59 -0300

    fixing error

--- game_scenes/Camp/camp_scene.gd
+++ game_scenes/Camp/camp_scene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var transition: AnimationPlayer = $Transition
+@onready var transition: AnimationPlayer = $CampUI/Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
