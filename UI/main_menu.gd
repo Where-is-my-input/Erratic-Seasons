875541186086b8bae1649b0f8064162bf15e1fcb
Kalécio Pereira <kalecio.santana@gmail.com>
2024-07-14 19:33:53 -0300

    feat: create basic menu UI

---
+++ UI/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://UI/settings.tscn")
+
+
+func _on_play_pressed():
+	pass
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://UI/credits.tscn")
