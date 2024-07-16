a26e7eace2426c76efbaa3ad2e9eb3c1962a366a
GsX <156434433+GsX0789@users.noreply.github.com>
2024-07-16 18:43:35 -0300

    OverWorldAdjustments
    Now the player can only walk in the demarcated area

--- game_scenes/OverWorld/OWCharacter/over_world_character.gd
+++ game_scenes/OverWorld/OWCharacter/over_world_character.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+@onready var camera_2d: Camera2D = $Camera2D
 var playerAxis = Vector2.ZERO as Vector2
 var playerSpeed = 200 as float
+var _stopped = false as bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,5 +19,10 @@ func MovePlayer(delta : float) -> void:
 		Input.get_axis("left","right"),
 		Input.get_axis("up", "down"),
 		)
-	velocity = playerAxis * playerSpeed
-	move_and_slide()
+	if(!_stopped):
+		velocity = playerAxis * playerSpeed
+		move_and_slide()
+
+func _on_feet_area_area_entered(area: Area2D) -> void:
+	_stopped = true
+	global_position = area.global_position
