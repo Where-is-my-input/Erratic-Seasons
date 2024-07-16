c19ac756843d15c012864f08cd7499ecdfe8b931
GsX <156434433+GsX0789@users.noreply.github.com>
2024-07-16 19:46:40 -0300

    Trade Scene
    Barebone of the trade scene

--- game_scenes/Trade/trade_scene.gd
+++ game_scenes/Trade/trade_scene.gd
@@ -5,3 +5,15 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	transition.play("fadeIn")
+
+
+func _on_buy_bt_pressed() -> void:
+	print("Opening buying menu!!")
+
+
+func _on_sell_bt_pressed() -> void:
+	print("Opening sell menu!")
+
+
+func _on_gb_bt_pressed() -> void:
+	get_tree().change_scene_to_packed(Global.OwScene)
