0ccdd6f52e5dae1de06b45453a92d556e66a5dad
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-07-20 15:46:36 -0300

    overworld sprites animation

--- game_scenes/OverWorld/OWCharacter/over_world_character.gd
+++ game_scenes/OverWorld/OWCharacter/over_world_character.gd
@@ -6,6 +6,8 @@ var playerSpeed = 200 as float
 var _stopped = false as bool
 var spawnPos : Vector2 = Vector2(404,296)
 var xOffset : float = 100
+@onready var spr_cecilia = $sprCecilia
+@onready var spr_geovanna = $sprGeovanna
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +20,32 @@ func _process(delta: float) -> void:
 	MovePlayer(delta)
 
 func MovePlayer(delta : float) -> void:
+	var dirX = Input.get_axis("left","right")
+	var dirY = Input.get_axis("up", "down")
 	playerAxis = Vector2(
-		Input.get_axis("left","right"),
-		Input.get_axis("up", "down"),
+		dirX,
+		dirY
 		)
 	if(!_stopped):
+		if playerAxis.x == 1:
+			setAnimation("right")
+		elif playerAxis.x == -1:
+			setAnimation("left")
+		elif playerAxis.y == -1:
+			setAnimation("up")
+		elif playerAxis.y == 1:
+			setAnimation("down")
+		else:
+			stopAnimation()
 		velocity = playerAxis * playerSpeed
 		move_and_slide()
+func setAnimation(anim):
+	spr_cecilia.play(anim)
+	spr_geovanna.play(anim)
+
+func stopAnimation():
+	spr_cecilia.pause()
+	spr_geovanna.pause()
 
 func _on_feet_area_area_entered(area: Area2D) -> void:
 	_stopped = true
